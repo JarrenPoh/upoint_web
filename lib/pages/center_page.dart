654518fc098ece0136e6c9a694846ec1b14d5520
JarrenPoh <jarrenpoh@gmail.com>
2024-02-26 01:40:49 +0800

    update center pic -20240224

--- lib/pages/center_page.dart
+++ lib/pages/center_page.dart
@@ -25,37 +25,37 @@ class _CenterPageState extends State<CenterPage> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 48),
-            child: Container(
-              height: widget.isWeb ? 1230 : null,
-              width: widget.isWeb ? 1076 : 543,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    height: 69,
-                    child: Row(
-                      children: [
-                        const SizedBox(width: 45),
-                        MediumText(
-                          color: grey500,
-                          size: 18,
-                          text: "活動中心",
-                        )
-                      ],
-                    ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 48),
+          child: Container(
+            height: widget.isWeb ? 1230 : double.infinity,
+            width: widget.isWeb ? 1076 : 543,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 69,
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 45),
+                      MediumText(
+                        color: grey500,
+                        size: 18,
+                        text: "活動中心",
+                      )
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 45,
-                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 45,
+                  ),
+                  child: SingleChildScrollView(
                     child: Column(
                       children: [
                         const SizedBox(height: 30),
@@ -64,97 +64,97 @@ class _CenterPageState extends State<CenterPage> {
                       ],
                     ),
                   ),
-                  Expanded(child: Column(children: [],)),
-                  ValueListenableBuilder<int>(
-                    valueListenable: widget.bloc.pageValueNotifier,
-                    builder: (context, value, child) {
-                      int _currPage = value;
-                      String leftImage = _currPage == 1
-                          ? "arrow_left_grey"
-                          : "arrow_left_primary";
-                      String rightImage = _currPage == widget.bloc.allPage
-                          ? "arrow_right_grey"
-                          : "arrow_right_primary";
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 60,
-                          vertical: 10,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            MouseRegion(
-                              cursor: SystemMouseCursors.click,
-                              child: GestureDetector(
-                                onTap: _currPage != 1
-                                    ? () => widget.bloc
-                                        .fetchNextPosts(_currPage - 1)
-                                    : () {},
-                                child: Container(
-                                  width: 27,
-                                  height: 27,
-                                  decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                      image: NetworkImage(
-                                        "assets/$leftImage.png",
-                                      ),
-                                      fit: BoxFit.cover,
+                ),
+                Expanded(child: Column(children: [],)),
+                ValueListenableBuilder<int>(
+                  valueListenable: widget.bloc.pageValueNotifier,
+                  builder: (context, value, child) {
+                    int _currPage = value;
+                    String leftImage = _currPage == 1
+                        ? "arrow_left_grey"
+                        : "arrow_left_primary";
+                    String rightImage = _currPage == widget.bloc.allPage
+                        ? "arrow_right_grey"
+                        : "arrow_right_primary";
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 60,
+                        vertical: 10,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          MouseRegion(
+                            cursor: SystemMouseCursors.click,
+                            child: GestureDetector(
+                              onTap: _currPage != 1
+                                  ? () => widget.bloc
+                                      .fetchNextPosts(_currPage - 1)
+                                  : () {},
+                              child: Container(
+                                width: 27,
+                                height: 27,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                      "assets/$leftImage.png",
                                     ),
+                                    fit: BoxFit.cover,
                                   ),
                                 ),
                               ),
                             ),
-                            const SizedBox(width: 12),
-                            Container(
-                              height: 23,
-                              width: 38,
-                              decoration: BoxDecoration(
-                                border: Border.all(color: grey300),
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                              child: Center(
-                                child: RegularText(
-                                  color: grey500,
-                                  size: 16,
-                                  text: _currPage.toString(),
-                                ),
-                              ),
+                          ),
+                          const SizedBox(width: 12),
+                          Container(
+                            height: 23,
+                            width: 38,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: grey300),
+                              borderRadius: BorderRadius.circular(5),
                             ),
-                            const SizedBox(width: 4),
-                            RegularText(
+                            child: Center(
+                              child: RegularText(
                                 color: grey500,
                                 size: 16,
-                                text: "/ ${widget.bloc.allPage}"),
-                            const SizedBox(width: 12),
-                            MouseRegion(
-                              cursor: SystemMouseCursors.click,
-                              child: GestureDetector(
-                                onTap: _currPage != widget.bloc.allPage
-                                    ? () => widget.bloc
-                                        .fetchNextPosts(_currPage + 1)
-                                    : () {},
-                                child: Container(
-                                  width: 27,
-                                  height: 27,
-                                  decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                      image: NetworkImage(
-                                        "assets/$rightImage.png",
-                                      ),
-                                      fit: BoxFit.cover,
+                                text: _currPage.toString(),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 4),
+                          RegularText(
+                              color: grey500,
+                              size: 16,
+                              text: "/ ${widget.bloc.allPage}"),
+                          const SizedBox(width: 12),
+                          MouseRegion(
+                            cursor: SystemMouseCursors.click,
+                            child: GestureDetector(
+                              onTap: _currPage != widget.bloc.allPage
+                                  ? () => widget.bloc
+                                      .fetchNextPosts(_currPage + 1)
+                                  : () {},
+                              child: Container(
+                                width: 27,
+                                height: 27,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                      "assets/$rightImage.png",
                                     ),
+                                    fit: BoxFit.cover,
                                   ),
                                 ),
                               ),
                             ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                  const SizedBox(height: 45),
-                ],
-              ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 45),
+              ],
             ),
           ),
         ),
--- lib/widgets/center/layouts/center_pic_layout.dart
+++ lib/widgets/center/layouts/center_pic_layout.dart
@@ -1,5 +1,7 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:upoint_web/color.dart';
+import 'package:upoint_web/globals/bold_text.dart';
 import 'package:upoint_web/models/post_model.dart';
 
 class CenterPicLayout extends StatefulWidget {
@@ -16,23 +18,62 @@ class CenterPicLayout extends StatefulWidget {
 }
 
 class _CenterPicLayoutState extends State<CenterPicLayout> {
+  bool isHover = false;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: widget.width,
-      decoration: BoxDecoration(
-        color: grey100,
-        image: DecorationImage(
-          image: NetworkImage(
-            widget.post.photo!,
+    return InkWell(
+      onHover: (value) {
+        setState(() {
+          isHover = value;
+        });
+      },
+      onTap: () {
+        print('tap');
+      },
+      child: Container(
+        width: widget.width,
+        decoration: BoxDecoration(
+          color: grey100,
+          image: DecorationImage(
+            image: NetworkImage(
+              widget.post.photo!,
+            ),
+            fit: BoxFit.cover,
           ),
-          fit: BoxFit.cover,
+          borderRadius: BorderRadius.circular(10),
         ),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: AspectRatio(
-        aspectRatio: 16 / 9,
-        child: Container(),
+        child: AspectRatio(
+          aspectRatio: 16 / 9,
+          child: isHover
+              ? ClipRect(
+                child: Container(
+                    decoration: BoxDecoration(
+                      color: grey500.withOpacity(0.4),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    alignment: Alignment.center,
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(
+                          sigmaX: 2.0, sigmaY: 2.0), 
+                      child: Container(
+                        width: 128,
+                        height: 35,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          border: Border.all(color: Colors.white),
+                        ),
+                        child: const BoldText(
+                          color: Colors.white,
+                          size: 16,
+                          text: "查看詳細內容",
+                        ),
+                      ),
+                    ),
+                  ),
+              )
+              : Container(),
+        ),
       ),
     );
   }
