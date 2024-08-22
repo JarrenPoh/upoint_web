8cbb3fa709ec43a799ef1004be214c1e19c8b451
JarrenPoh <jarrenpoh@gmail.com>
2024-08-23 06:14:53 +0800

    FIX: 頁籤欄全自中對齊＋調整字體

--- lib/widgets/custom_navigation_bar.dart
+++ lib/widgets/custom_navigation_bar.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:upoint_web/color.dart';
 import 'package:upoint_web/firebase/auth_methods.dart';
+import 'package:upoint_web/globals/medium_text.dart';
 import 'package:upoint_web/globals/regular_text.dart';
 import 'dart:html';
 import '../globals/custom_messengers.dart';
@@ -210,11 +211,14 @@ class CustomNavigationBarState extends State<CustomNavigationBar> {
     double screenWidth = MediaQuery.of(context).size.width;
     if (screenWidth <= 500) {
       return mobileLayout();
+    } else if (screenWidth <= 1080) {
+      return webLayout(isWeb: false); // 平板布局
     } else {
-      return webLayout();
+      return webLayout(isWeb: true); // 桌面布局
     }
   }
 
+  // 手機上使用的
   Widget mobileLayout() {
     return Container(
       height: 120,
@@ -244,7 +248,7 @@ class CustomNavigationBarState extends State<CustomNavigationBar> {
                   style: TextStyle(
                     fontSize: 24,
                     color: primaryColor,
-                    fontFamily: 'NotoSansBold',
+                    fontFamily: 'NotoSansBlack',
                   ),
                 ),
                 TextSpan(
@@ -252,7 +256,7 @@ class CustomNavigationBarState extends State<CustomNavigationBar> {
                   style: TextStyle(
                     fontSize: 24,
                     color: grey500,
-                    fontFamily: 'NotoSansBold',
+                    fontFamily: 'NotoSansBlack',
                   ),
                 ),
                 if (widget.isForm)
@@ -290,7 +294,8 @@ class CustomNavigationBarState extends State<CustomNavigationBar> {
     );
   }
 
-  Widget webLayout() {
+  // 網站上使用的
+  Widget webLayout({required bool isWeb}) {
     return Stack(
       children: [
         Container(
@@ -306,103 +311,91 @@ class CustomNavigationBarState extends State<CustomNavigationBar> {
               ),
             ],
           ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const Expanded(
-                        flex: 5,
-                        child: Column(
-                          children: [],
-                        ),
-                      ),
-                      RichText(
-                        text: TextSpan(
-                          style: DefaultTextStyle.of(context).style,
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'U',
-                              style: TextStyle(
-                                fontSize: 24,
-                                color: primaryColor,
-                                fontFamily: 'NotoSansBold',
-                              ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: isWeb == true ? 1076 : 543,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        style: DefaultTextStyle.of(context).style,
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'U',
+                            style: TextStyle(
+                              fontSize: 24,
+                              color: primaryColor,
+                              fontFamily: 'NotoSansBlack',
                             ),
+                          ),
+                          TextSpan(
+                            text: 'Point',
+                            style: TextStyle(
+                              fontSize: 24,
+                              color: grey500,
+                              fontFamily: 'NotoSansBlack',
+                            ),
+                          ),
+                          if (!widget.isForm && isWeb)
                             TextSpan(
-                              text: 'Point',
+                              text: ' 活動管理',
                               style: TextStyle(
-                                fontSize: 24,
+                                fontSize: 22,
                                 color: grey500,
-                                fontFamily: 'NotoSansBold',
+                                fontFamily: 'NotoSansMedium',
                               ),
                             ),
-                            if (widget.isForm)
-                              TextSpan(
-                                text: " 報名系統",
-                                style: TextStyle(
-                                  fontSize: 22,
-                                  color: grey500,
-                                  fontFamily: 'NotoSansMedium',
-                                ),
-                              ),
-                          ],
-                        ),
-                      ),
-                      const Expanded(
-                        flex: 5,
-                        child: Column(
-                          children: [],
-                        ),
-                      ),
-                      if (!widget.isForm)
-                        Row(
-                          children: List.generate(
-                            tapContainerList.length,
-                            (index) {
-                              return Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 20),
-                                child: tapContainer(
-                                  tapContainerList,
-                                  index,
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                      if (FirebaseAuth.instance.currentUser != null)
-                        MouseRegion(
-                          cursor: SystemMouseCursors.click,
-                          child: GestureDetector(
-                            onTap: () {
-                              _toggleOverlay();
-                            },
-                            child: SizedBox(
-                              height: 40,
-                              width: 40,
-                              child: Image.asset(
-                                "assets/profile.png",
-                                fit: BoxFit.cover,
+                          if (widget.isForm)
+                            TextSpan(
+                              text: " 報名系統",
+                              style: TextStyle(
+                                fontSize: 22,
+                                color: grey500,
+                                fontFamily: 'NotoSansMedium',
                               ),
                             ),
-                          ),
-                        ),
-                      const Expanded(
-                        flex: 1,
-                        child: Column(
-                          children: [],
+                        ],
+                      ),
+                    ),
+                    if (!widget.isForm)
+                      Row(
+                        children: List.generate(
+                          tapContainerList.length + 1,
+                          (index) {
+                            if (index + 1 == tapContainerList.length + 1)
+                              return FirebaseAuth.instance.currentUser != null
+                                  ? MouseRegion(
+                                      cursor: SystemMouseCursors.click,
+                                      child: GestureDetector(
+                                        onTap: () {
+                                          _toggleOverlay();
+                                        },
+                                        child: SizedBox(
+                                          height: 40,
+                                          width: 40,
+                                          child: Image.asset(
+                                            "assets/profile.png",
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  : Container();
+                            return tapContainer(
+                              tapContainerList,
+                              index,
+                            );
+                          },
                         ),
                       ),
-                    ],
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
         Container(
@@ -419,6 +412,7 @@ class CustomNavigationBarState extends State<CustomNavigationBar> {
     );
   }
 
+  // 頁籤
   Widget tapContainer(List list, int index) {
     return InkWell(
       onHover: (value) {
@@ -442,19 +436,19 @@ class CustomNavigationBarState extends State<CustomNavigationBar> {
         });
       },
       child: Container(
-        height: 37,
-        // width: 86,
-        padding: EdgeInsets.symmetric(horizontal: 8),
+        height: index == 2 ? 37 : null,
+        padding: EdgeInsets.symmetric(horizontal: index == 2 ? 8 : 20),
+        margin: EdgeInsets.symmetric(horizontal: index == 2 ? 14 : 0),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(index == 2 ? 10 : 0),
           color: index == 2
               ? list[index]['isHover']
                   ? secondColor
                   : primaryColor
               : list[index]['isSelected']
-                  ? grey200
+                  ? grey100
                   : list[index]['isHover']
-                      ? grey200
+                      ? grey100
                       : Colors.white,
         ),
         child: Center(
@@ -467,7 +461,7 @@ class CustomNavigationBarState extends State<CustomNavigationBar> {
                   size: 24,
                 ),
               SizedBox(width: 4),
-              RegularText(
+              MediumText(
                 color: index == 2 ? Colors.white : Colors.black,
                 size: 16,
                 text: list[index]['title'],
